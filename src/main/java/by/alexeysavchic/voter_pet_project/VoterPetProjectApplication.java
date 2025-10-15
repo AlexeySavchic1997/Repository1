a936e9569a2dc63282d1176ed98a258ead25feb6
Alexey Savchic <savchic7991@gmail.com>
2025-10-15 14:21:19 +0300

    Initial Commit

---
+++ src/main/java/by/alexeysavchic/voter_pet_project/VoterPetProjectApplication.java
@@ -0,0 +1,13 @@
+package by.alexeysavchic.voter_pet_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoterPetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoterPetProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/alexeysavchic/voter_pet_project/VoterPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.alexeysavchic.voter_pet_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoterPetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
